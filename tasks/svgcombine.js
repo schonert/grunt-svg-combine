8300407cabdaf552059574ed074905eb49457e76
Stefan Schonert <sschonert@skybrud.dk>
2014-05-15 14:48:48 +0200

    reg update

--- tasks/svgcombine.js
+++ tasks/svgcombine.js
@@ -10,16 +10,16 @@
 
 var cheerio = require('cheerio'), // Serverside jQery - used to change id
 	SVGO = require('svgo'), // Minify svg
-	path = require('path'); // Gets file name from path
+	path = require('path'), // Gets file name from path
+	prettyBytes = require('pretty-bytes'); 
 
 module.exports = function(grunt) {
 
-	var writtenFiles = {};
+	var writtenFiles = [];
 
 	// Add task
 	grunt.registerMultiTask('svgcombine', 'Combine multiple svg files in one single file.', function() {
 		
-
 		// Default options
 		var options = this.options({
 			prefix: '', // Prefix the stored svg id
@@ -34,9 +34,20 @@ module.exports = function(grunt) {
 		// Determin filter
 		var filter = options.filter;
 
+		// Total amount of files
+		var totalFiles = 0;
+
+		// bytes saved
+		var bytesSaved = 0;
+
+		grunt.log.writeln();
+		grunt.log.subhead('Running ' + this.target);
+
 		// Each destination
 		this.files.forEach(function(f) {
 			var destinationContent = ''; // Contains svg's
+			
+			totalFiles += f.src.length;
 		
 			// Check path
 			f.src.filter(function(filePath){
@@ -44,10 +55,12 @@ module.exports = function(grunt) {
 				// Check if files exist
 				if (!grunt.file.exists(filePath)) {
 					grunt.log.warn('Dooh! "' + filePath + '" ain\'t there!.');
+					totalFiles--;
 					return false;
 				} else {
 					return true;
 				}
+
 				
 			})
 			// Loop through src files
@@ -76,21 +89,31 @@ module.exports = function(grunt) {
 					filter(svg, fileName, filePath);
 				}
 
+				grunt.log.writeln(filePath + ' (' + prettyBytes(svgContent.length) + ')');
+
 				// Stores svg with linebreak
 				destinationContent += svg + '\n';
+
+				bytesSaved += svgContent.length - svg.html().length;
 
 			});
 
 			// Save
-			if(options.append && writtenFiles[f.dest]){
+			if(options.append && writtenFiles.indexOf(f.dest) !== -1){
 				var temp = grunt.file.read(f.dest);
 				grunt.file.write(f.dest, temp + destinationContent);
 			}else{
 				grunt.file.write(f.dest, destinationContent);
-				writtenFiles[f.dest] = true;
+				writtenFiles.push(f.dest);
 			}
+
 			// Finally done!
-			grunt.log.writeln('There you go my friend "' + f.dest + '" was created with specially for you ♥');
+			grunt.log.writeln('\nThere you go my friend "' + f.dest + '" was created specially for you ♥');
+
+
+
 		});
+			grunt.log.subhead('A total of ' + prettyBytes(bytesSaved) + ' and ' + (totalFiles-writtenFiles.length)+ ' requests where saved' );
+
 	});
 };
